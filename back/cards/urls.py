1eb92fc481ed6802babcbc4ff31385209c534f27
lim kyoung tae <limkt0925@gmail.com>
2024-05-17 19:25:57 +0900

    team project day2 - backend

--- back/cards/urls.py
+++ back/cards/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 크롤링
+    # 크롤링 및 DB에 저장
     path('card_gorilla_selenium/', views.card_gorilla_selenium),
+    path('csv_to_db/', views.csv_to_db),
     # 카드
     path('', views.card_list),
     path('<int:card_pk>/', views.card_detail),
@@ -11,5 +12,5 @@ urlpatterns = [
     path('<int:card_pk>/review/<int:review_pk>/', views.review),
     path('favorite/', views.favorite),
     #카드 추천
-    path('<str:username>/card_recommend/', views.card_recommend),
+    path('card_recommend/', views.card_recommend),
 ]
